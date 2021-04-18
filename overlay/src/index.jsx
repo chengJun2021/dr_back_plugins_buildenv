4dd9eee85cd4269fb2bff9764824d14064dd3b16
matt253657 <matthew.chong@datareachable.com>
2021-04-18 14:46:29 +1000

    :sparkles: include `raw-loader` 2021-04-18

--- overlay/src/index.jsx
+++ overlay/src/index.jsx
@@ -3,6 +3,7 @@ import './index.scss';
 import "../package.json";
 import * as ReactDOM from "react-dom";
 import includeme from "./includeme.png";
+import thicc from "./thicc.png";
 
 const TestyMcTestface = () => <div className="red-text">Test</div>
 
@@ -10,5 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(<div>
         <TestyMcTestface/>
         <img src={includeme} alt="webpack logo, experimenting with loading images atm"/>
+        <img src={thicc} alt="a very big image to stress test webpack"/>
     </div>, document.body)
 })
